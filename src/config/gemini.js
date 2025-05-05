ad7d6c675fc7193ff9c3ca42b2f36c19cbb3594b
nibedika gautam <nibedika35@gmail.com>
2025-05-05 19:20:47 +0545

    Add gemini config

---
+++ src/config/gemini.js
@@ -0,0 +1,52 @@
+
+const {
+    GoggleGenerateiveAi,
+    HarmCategory,
+    HarmBlockThreshold,
+} = require("@goggle/generative-ai");
+
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const MODEL_NAME = "gemini-0.1-pro";
+const API_KEY = "AIzaSyCvkaNsEcNUKRYbUn_v1eMpylIn8-ATDUE";
+
+async function runChat(prompt){
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel({ model: MODEL_NAME});
+
+    const generationConfig = {
+        temperature: 0.9,
+        topK: 1,
+        topP,
+        maxOutputTokens: 2048,
+    };
+
+    const safetySettings = [
+        {
+            category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+        {
+            category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+        {
+            category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+    ];
+
+    const chat = model.startChat({
+        generationConfig,
+        safetySettings,
+        history: [
+
+        ],
+    });
+
+    const result = await chat.sendMessage(prompt);
+    const response = result.response;
+    console.log(response.text());
+}
+
+export default runChat;
